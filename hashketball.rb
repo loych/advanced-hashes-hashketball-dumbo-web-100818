54434d15f00b4b48949bfe71fdb561b6d356eea5
Lois Choi <insil321@hotmail.com>
2018-10-06 21:42:48 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -101,7 +101,7 @@ def big_shoe_rebounds
   current_rebound=0
   game_hash.each do |key, value|
     value[:players].each do |player, data|
-     if data[:shoe] > current_size
+     if data[:shoe]>current_size
           data[:shoe] = current_size
           current_rebound = data[:rebounds]
           end
